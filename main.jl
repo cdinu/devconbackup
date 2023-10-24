0ab606c77e625c39b9a8ebab2761ad6b7ec4bc6d
Cristian Dinu <cristian.dinu@learnfwd.com>
2023-10-24 10:14:43 +0000

    tau

--- main.jl
+++ main.jl
@@ -1,13 +1,13 @@
+using Pkg
+Pkg.add("Plots") # this might take a bit
+
 function hello()
     println("Wϵlcomϵ to the Julia demo!")
 end
 
 hello()
 
-using Pkg
-Pkg.add("Plots") # this might take a bit
-
 using Plots
 x = 0:0.1:2π
-y = sin.(x)
-plot(x, y, label="sin(x)", title="Sin Wave", xlabel="x", ylabel="y")
+τ = sin.(x)
+plot(x, τ, label="sin(x)", title="Sin Wave", xlabel="x", ylabel="Sin")
